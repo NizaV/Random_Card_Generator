c1e08cbfe7cf561c08c60bccfb0a2837045f08aa
Niza Vera <nizavera95@gmail.com>
2020-06-12 01:07:33 +0000

    Initial Commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,61 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var suits = [
+    {
+      suit: "♣︎",
+      color: "black"
+    },
+    {
+      suit: "♥︎",
+      color: "red"
+    },
+    {
+      suit: "♠︎",
+      color: "black"
+    },
+    {
+      suit: "♦︎",
+      color: "red"
+    }
+  ];
+  var value = values[this.Math.floor(Math.random() * values.length)];
+  var suit = suits[this.Math.floor(Math.random() * suits.length)];
+
+  var card = document.createElement("div");
+  var suitUpper = document.createElement("div");
+  var suitLower = document.createElement("div");
+  var valueMiddle = document.createElement("div");
+
+  card.classList.add("card");
+  suitUpper.classList.add("suit");
+  suitLower.classList.add("suit", "right");
+  valueMiddle.classList.add("value");
+
+  valueMiddle.innerHTML = value;
+  suitUpper.innerHTML = suit.suit;
+  suitUpper.style.color = suit.color;
+  suitLower.innerHTML = suit.suit;
+  suitLower.style.color = suit.color;
+
+  card.appendChild(suitUpper);
+  card.appendChild(valueMiddle);
+  card.appendChild(suitLower);
+
+  document.body.appendChild(card);
 };
